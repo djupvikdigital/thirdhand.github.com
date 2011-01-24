665baa26b94af776b6c4303cb8250fed4187fcc5
Reidar Djupvik <mail@3rdhand.info>
2011-01-24 22:54:24 +0100

    Change keypress to keydown.

--- richform.js
+++ richform.js
@@ -194,7 +194,7 @@
 				select.hideMenu();
 			});
 			// Prevent pressing enter in the menu from submitting the form
-			select.menu.keypress(function(e) {
+			select.menu.keydown(function(e) {
 				if(e.which == 13) e.preventDefault();
 			});
 			select.valstore.change(function() {
